c3ea4b72c41b4561c018e13a2a99b8b527a19ff7
huangyong <braveryhuang@126.com>
2025-04-19 20:26:05 +0800

    Task1 实现aggregate，熟悉一些函数的基本用法
    groupBy, map

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -15,4 +15,27 @@ TODO: Write aggregation code.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
 fun List<User>.aggregate(): List<User> =
-    this
+    groupBy { it.login }
+        .map { (login, group) -> User(login, group.sumOf { it.contributions }) }
+        .sortedByDescending { it.contributions }
+
+
+//简单看下 groupBy 和 map 的实现
+fun <T, K> Iterable<T>.customGroupBy(keySelector: (T) -> K): Map<K, List<T>> {
+    val result = mutableMapOf<K, MutableList<T>>()
+    for (element in this) {
+        val key = keySelector(element)
+        val list = result.getOrPut(key) { mutableListOf() }
+        list.add(element)
+    }
+    return result
+}
+
+//map 对应List和Map有不同的实现（参数类型不同，Map对应的是Map.Entry）
+fun <T, R> Iterable<T>.customMap(transform: (T) -> R): List<R> {
+    val result = mutableListOf<R>()
+    for (element in this) {
+        result.add(transform(element))
+    }
+    return result
+}
