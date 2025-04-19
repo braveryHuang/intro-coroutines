b2769fe7bd66520c804d11b174d40c9dad43ea63
huangyong <braveryhuang@126.com>
2025-04-19 21:56:03 +0800

    Task3 Callback的潜在问题，不容易修改，易错；并发引起的同步问题

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -5,22 +5,41 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
 
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     service.getOrgReposCall(req.org).onResponse { responseRepos ->
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
-        val allUsers = mutableListOf<User>()
+        val allUsers = Collections.synchronizedList(mutableListOf<User>())
+        val numberOfProcessed = AtomicInteger()
+
         for (repo in repos) {
             service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
                 allUsers += users
+                if (numberOfProcessed.incrementAndGet() == repos.size) {
+                    updateResults(allUsers.aggregate())
+                }
             }
         }
-        // TODO: Why this code doesn't work? How to fix that?
-        updateResults(allUsers.aggregate())
+
+        //一个更好的方案，使用countDownLatch
+        /*val countDownLatch = CountDownLatch(repos.size)
+        for (repo in repos) {
+            service.getRepoContributorsCall(req.org, repo.name)
+                .onResponse { responseUsers ->
+                    // processing repository
+                    countDownLatch.countDown()
+                }
+        }
+        countDownLatch.await()
+        updateResults(allUsers.aggregate())*/
+
+        //改完这个Callback的Task，你会发现使用Callback并不简单非常容易出错
+
     }
 }
 
